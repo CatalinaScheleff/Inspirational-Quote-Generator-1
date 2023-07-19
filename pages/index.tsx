4ea6549212224858103785895efacb0708b4e0d6
CatalinaScheleff <catascheleff@gmail.com>
2023-07-19 12:30:50 -0400

    actualizations

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
+import { GradientBackgroundCon } from '@/QuoteGenerator/QuoteGeneratorElements'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
 
 
 export default function Home() {
@@ -13,6 +13,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
        {/* Background */}
+       <GradientBackgroundCon>
+        
+       </GradientBackgroundCon>
     </>
   )
 }
