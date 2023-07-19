958d03812915e4409baf3eeda4a830b654c71191
CatalinaScheleff <catascheleff@gmail.com>
2023-07-19 12:00:45 -0400

    setting up

---
+++ QuoteGenerator/QuoteGeneratorElements.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import Link from "next/link";
+import Image from "next/image";
+
+export const GradientBackgroundCon = styled.div`
+    background: linear-gradient(to right, #000046, #1CB5E0, ...);
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from 'react'
-
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 
-export default function Home() {
 
+export default function Home() {
   return (
     <>
-      
+      <Head>
+        <title>Inspirational Quote Generator</title>
+        <meta name="description" content="A fun project to generate quotes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+       {/* Background */}
     </>
   )
 }
